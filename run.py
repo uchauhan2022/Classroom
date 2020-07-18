022673cf0ed23b6f43214806dc9db8fd136c068e
robinsandhu <er.robinsandhu@gmail.com>
2020-07-18 20:45:06 +0530

    Cleared index.html and moved to templates

--- run.py
+++ run.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return  render_template('index.html')
-
+# def index():
+#     return '<h1>Now it is Working!</h1>'
 
 
 if(__name__) == "__main__":
