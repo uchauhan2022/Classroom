d539414961f4933d53d596546d236f1168420082
uchauhan2022 <utkarshchauhan2022@gmail.com>
2020-07-19 18:22:25 +0530

    Initialize

--- run.py
+++ run.py
@@ -7,9 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return  render_template('index.html')
-# def index():
-#     return '<h1>Now it is Working!</h1>'
 
+@app.route('/login')
+def login():
+    return  render_template('login.html')
 
 if(__name__) == "__main__":
     app.run()
