4da6e6e2c24fef9f746ce23c4cee72bacf9548c3
uchauhan2022 <utkarshchauhan2022@gmail.com>
2020-07-19 20:02:12 +0530

    database reinitialzed

--- run.py
+++ run.py
@@ -1,4 +1,17 @@
-from flask import Flask,render_template
+from flask import Flask , render_template, g, request, url_for,redirect, session
+# import sqlite3
+# import requests
+# import json
+# import smtplib, ssl, glob
+# from email import encoders
+# from email.mime.base import MIMEBase
+# from email.mime.multipart import MIMEMultipart
+# from email.mime.text import MIMEText
+# from database import get_db
+# import hashlib
+# import requests
+# import os
+# from random import randint
 
 
 app = Flask(__name__)
@@ -10,7 +23,23 @@ def home():
 
 @app.route('/login')
 def login():
+
     return  render_template('login.html')
+
+@app.route('/signup', methods = ['GET','POST'])
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/logout')
+def logout():
+    user = get_current_user()
+    if user:
+        session.pop('username', None)
+        return redirect(url_for('login'))
+    else:
+        return redirect(url_for('login'))
+
 
 if(__name__) == "__main__":
     app.run()
