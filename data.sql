dcd010c7766ff4ab1e3fb51f0eeabded9310d08d
uchauhan2022 <utkarshchauhan2022@gmail.com>
2020-07-20 10:01:37 +0530

    corrections

--- data.sql
+++ data.sql
@@ -1,13 +1,11 @@
 create table users (
   id integer primary key autoincrement,
-  first_name varchar(100) not null,
-  last_name varchar(100),
-  username varchar(100) not null,
+  name varchar(100) not null,
   email varchar(100) not null,
   password varchar(100) not null,
   activation_status integer default 0,
   token varchar(100) not null,
-  role integer default 1
+  role varchar(100) not null
 );
 -- for super admin role = 69
 -- for teacher role = 420
--- run.py
+++ run.py
@@ -19,11 +19,43 @@ app = Flask(__name__)
 app.config['DEBUG']=True
 app.config['SECRET_KEY']= os.urandom(24)
 
+# FUNCTIONS
+def authenticator(email, token):
+    user_result = None
+    db = get_db()
+    cur = db.execute('select "name","email" from users where "email" = ?;',[email])
+    user_result = cur.fetchone()
+    name = user_result['name']
+    subject = "Verify Email Address"
+    sender_email = "isboomboomboom@gmail.com"
+    receiver_email = user_result['email']
+    temp_email = hashlib.md5(receiver_email.encode())
+    password = "Test_1234#"
+    message = MIMEMultipart()
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    message["Subject"] = subject
+    message["Bcc"] = receiver_email
+    bodytext = "Click on the link to verify account :"
+    link = "http://127.0.0.1:5000/verify?a="+token+"&b="+str(temp_email.hexdigest())
+    body = bodytext+link
+    message.attach(MIMEText(body, "plain"))
+    text = message.as_string()
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, text)
+
+
+
+
+
+
+# ROUTES
 @app.teardown_appcontext
 def close_db(error):
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
-
 
 @app.route('/')
 def home():
@@ -31,25 +63,50 @@ def home():
 
 @app.route('/login')
 def login():
-
     return  render_template('login.html')
+
+@app.route('/verify', methods=['GET','POST'])
+def verify():
+    token=request.args.get('a')
+    emailgot = request.args.get('b')
+    if request.method == 'POST':
+        username = request.form['login-username']
+        password = hashlib.md5(request.form['login-password'].encode())
+        token = request.form['token']
+        db = get_db()
+        cur = db.execute('select "id","name","username","email","password","token" from users where "username" = ?;',[username])
+        result=cur.fetchone()
+        if result:
+            if result['password']==password.hexdigest() and token==result['token'] :
+                session['username'] = username
+                f=get_db()
+                f.execute('update users set "activation_status" = 1 where username=? ', [username])
+                f.commit()
+                return  redirect(url_for('dashboard'))
+            else :
+                return "fail1"#render_template('login.html',flag=0)
+        else:
+            return "fail2"#render_template('login.html',flag=0)
+    return render_template('authenticate.html',flag = 1,token=token,emailgot=emailgot)
+
+
 
 @app.route('/signup', methods = ['GET','POST'])
 def signup():
     if request.method == 'POST':
         name = request.form['signup-name']
-        username = request.form['signup-username']
+        role = request.form['signup-role']
         email = request.form['signup-email']
         password = hashlib.md5(request.form['signup-password'].encode())
         token = hashlib.md5(str(randint(1,1000000)).encode())
         db = get_db()
-        cur = db.execute('select "id","name","username","email","password" from users where "username" = ?;',[username])
+        cur = db.execute('select * from users where "email" = ?;',[email])
         result=cur.fetchone()
         if result:
             return render_template('signup.html',flag = 0)
-        db.execute('insert into users ("name","username","email","password","token") values (?,?,?,?,?)',[name,username,email,password.hexdigest(),token.hexdigest()])
+        db.execute('insert into users ("name","email","password","token","role") values (?,?,?,?,?)',[name,email,password.hexdigest(),token.hexdigest(),role])
         db.commit()
-        authenticator(username,token.hexdigest())
+        authenticator(email,token.hexdigest())
         return redirect(url_for('login'))
     return render_template('signup.html', flag = 1)
 
@@ -57,7 +114,7 @@ def signup():
 def logout():
     user = get_current_user()
     if user:
-        session.pop('username', None)
+        session.pop('email', None)
         return redirect(url_for('login'))
     else:
         return redirect(url_for('login'))
